82a23b4b971f9ebe33fa5ad565d1aae426735618
Marcello Bozzi <marcello.bozzi@istruzione.it>
2020-03-24 12:35:03 +0100

    LightweightRP

---
+++ 2DSandbox/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(xAxis, yAxis, 0f);
+
+        rb.MovePosition(transform.position + direction);
+    }
+}
